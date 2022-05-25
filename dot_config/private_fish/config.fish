f7eaf2488cdfecf45d30d786abf9dcfe7e0a1f95
David Park <dpjungmin@gmail.com>
2022-05-25 01:22:20 -0700

    Add fish shell config

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,28 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias ls="exa -F"
+alias la="exa -aF"
+alias ll="exa -lF"
+alias lla="exa -alF"
+alias tree="exa --tree"
+alias cat=bat
+alias vim=nvim
+alias c=cargo
+alias cz=chezmoi
+
+set -x PATH $PATH /usr/local/bin
+set -x PATH $PATH ~/.local/bin
+set -x PATH $PATH ~/.cargo/bin
+set -x PATH $PATH ~/.emacs.d/bin
+
+switch (uname)
+    case Linux
+    case Darwin
+        set -x PATH $PATH /opt/brew/bin
+    case '*'
+end
+
+zoxide init fish | source
+starship init fish | source
