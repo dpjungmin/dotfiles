5f8b1cea07379d881fac3cbbe2fe74de77f55c7f
David Park <dpjungmin@gmail.com>
2022-06-17 10:41:29 -0700

    fish: add utility function

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -28,5 +28,20 @@ switch (uname)
     case '*'
 end
 
+# This will run the mkdir command, and if it is successful, change the current
+# working directory to the one just created.
+function mkcd -d "Create a directory and set CWD"
+    command mkdir $argv
+    if test $status = 0
+        switch $argv[(count $argv)]
+            case '-*'
+
+            case '*'
+                cd $argv[(count $argv)]
+                return
+        end
+    end
+end
+
 zoxide init fish | source
 starship init fish | source
